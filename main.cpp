48cf8d7a650da0c82957bea0ad8346b42807710a
Thuniinae <thunia4182@gmail.com>
2023-05-22 00:05:33 +0800

    [modify] DMA = true

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ static volatile uint32_t * const DMA_OP_ADDR   = (uint32_t * const)0x7000000C;
 static volatile uint32_t * const DMA_STAT_ADDR = (uint32_t * const)0x70000010;
 static const uint32_t DMA_OP_MEMCPY = 1;
 
-bool _is_using_dma = false;
+bool _is_using_dma = true;
 int read_bmp(std::string infile_name) {
   FILE *fp_s = NULL; // source file handler
   fp_s = fopen(infile_name.c_str(), "rb");
